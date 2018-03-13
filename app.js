f53f805a581696b746b0f8aa11d5173016104e1f
nattaaek <w.nattaaek@yahoo.com>
2018-03-13 20:53:14 +0700

    rebot

--- app.js
+++ app.js
@@ -1,112 +1,84 @@
-/*-----------------------------------------------------------------------------
-A simple echo bot for the Microsoft Bot Framework. 
------------------------------------------------------------------------------*/
-
 var restify = require('restify');
 var builder = require('botbuilder');
-var botbuilder_azure = require("botbuilder-azure");
+var azure = require('azure-storage');
+var path = require('path');
+var jsdom = require('jsdom');
+var uuid = require('uuid');
+var util = require('util');
+var fs = require('fs');
+const { JSDOM } = jsdom;
+const { window } = new JSDOM();
+const { document } = (new JSDOM('')).window;
+global.document = document;
 
-// Setup Restify Server
+var $ = jQuery = require('jquery')(window);
+
+var connectionString = 'DefaultEndpointsProtocol=https;AccountName=testblobza;AccountKey=FlewVgyJzhZntjqPuZ8BO3d9Us4Blh/fG6wRFqrfKE6SG+tQd7PxYhp1E+uBInC8UW8d3M/EhyfQzqe3SYlPfA==;EndpointSuffix=core.windows.net';
+var blobService = azure.createBlobService(connectionString);
+var USER_HOME = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+var DOCUMENT_FOLDER = path.join(USER_HOME, 'Documents');
+if (!fs.existsSync(DOCUMENT_FOLDER)) {
+  fs.mkdirSync(DOCUMENT_FOLDER);
+}
+
+var container = 'taskcontainer';
+
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
-  
-// Create chat connector for communicating with the Bot Framework Service
+
 var connector = new builder.ChatConnector({
     appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword,
-    openIdMetadata: process.env.BotOpenIdMetadata
+    appPassword: process.env.MicrosoftAppPassword
 });
 
-// Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
-/*----------------------------------------------------------------------------------------
-* Bot Storage: This is a great spot to register the private state storage for your bot. 
-* We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
-* For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
-* ---------------------------------------------------------------------------------------- */
 
-var tableName = 'botdata';
-var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
-var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
-
-// Create your bot with a function to receive messages from the user
-var bot = new builder.UniversalBot(connector);
-bot.set('storage', tableStorage);
-
-var bot = new builder.UniversalBot(connector, function (session) {
-
-    var str = [...session.message.text];
-    var stack = [];
-    var first = 9999999, last = 0;
-    for(var i = 0 ; i < str.length; i++){
-        if(EmptyStack(last) && isOpenBracket(str[i])) {
-            stack[last] = str[i];
-            last++;
-            first = 0;
-        }
-        else if(isOpenBracket(str[i])) {
-            if(str[i] == '(' && stack[last-1] == '{' || stack[last-1] == '(') {
-                stack[last] = str[i];
-                last++;
-            }
-            else if(str[i] == '[' && stack[last-1] == '(') {
-                stack[last] = str[i];
-                last++;
-            }
-            else if(str[i] == '{' && stack[last-1] == '{') {
-                stack[last] = str[i];
-                last++;
-            }
-        }
-        else if(isCloseBracket(str[i])){
-            if(first != 0){
-                break;
-            }
-            else {
-                if(str[i].charCodeAt() - 1 == stack[last-1].charCodeAt() || str[i].charCodeAt() - 2 == stack[last-1].charCodeAt()) {
-                    last--;
-                }
-            }
-        }
-    }
- 
-
-    if(last == first){
-        session.send("You said: %s it's balance bracket Nice Guy", session.message.text);
-    }
-    else{
-        session.send("error: %s syntax is error please fix it", session.message.text);
-    }
-
-});
-
-function EmptyStack(last) {
+    var bot = new builder.UniversalBot(connector, function(session) {
+        var msg = session.message;
+        if(msg.attachments && msg.attachments.length > 0) {
+            var attachment = msg.attachments[0];
+            var blobName = attachment.name;
+            blobService.createBlockBlobFromLocalFile(container,blobName,attachment.name, function(error) {
+                    handleError(error);
+                });
     
-    if (last == 0) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
+        var blobUrl = blobService.getUrl(container,blobName);
+    
+        var subscriptionKey = "585ac8068c574ee99be927d0b5d204ce";
+        var uriBase = "https://southcentralus.api.cognitive.microsoft.com/customvision/v1.1/Prediction/eeb89498-aa9a-48b0-8404-c4a9ad90cbaf/url";
 
-function isOpenBracket(inputChar) {
-    if(inputChar == '(' || inputChar == '[' || inputChar == '{') {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
+      $.ajax({
+        url: uriBase,
 
-function isCloseBracket(inputChar) {
-    if(inputChar == ')' || inputChar == ']' || inputChar == '}') {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
+        // Request headers.
+        beforeSend: function(xhrObj){
+            xhrObj.setRequestHeader("Content-Type","application/json");
+            xhrObj.setRequestHeader("Prediction-Key", subscriptionKey);
+        },
+
+        type: "POST",
+
+        // Request body.
+        data: '{"url": ' + '"' + blobUrl + '"}',
+    })
+    .done(function(data) {
+        var json = data.Predictions[0].Tag;
+        session.send("user said:" + json);
+    })
+            
+        }
+        else {
+            session.send("you said %s", msg.text);
+        }
+    });
+
+
+function handleError(error) {
+        if (error) {
+          console.error('Exception thrown:\n', error);
+          process.abort();
+        }
+      }
